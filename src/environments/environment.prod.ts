ec47252dac66433a3fd7840594c84d8e360a42bc
dpdhruv <dhruv_7693@yahoo.co.in>
2018-09-07 16:27:44 +0530

    fix-firebase-issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyB97GEt7jzCFxAgvz6QRmxUlAK_PzJg6cs",
+    authDomain: "egovernance-81e95.firebaseapp.com",
+    databaseURL: "https://egovernance-81e95.firebaseio.com",
+    projectId: "egovernance-81e95",
+    storageBucket: "egovernance-81e95.appspot.com",
+    messagingSenderId: "884357908409"
+  }
 };
